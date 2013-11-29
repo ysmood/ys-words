45529a6e96dd5e00259f6318905d67e14f7015b5
Yad Smood <y.s.inside@gmail.com>
2013-11-30 03:15:40 +0800

    opt: Server control.

---
+++ kit/server_ctl.sh
@@ -0,0 +1,16 @@
+coffee='node_modules/.bin/coffee'
+forever='node_modules/.bin/forever'
+
+app='app.coffee'
+
+case $1 in
+	'start' )
+		uptime_conf='--minUptime 5000 --spinSleepTime 5000'
+		log_conf='-a -o log/std.log -e log/err.log'
+		$forever start $uptime_conf $log_conf -c $coffee $app
+		;;
+
+	'stop' )
+		$forever stop $app
+		;;
+esac
--- kit/start.sh
+++
@@ -1,6 +0,0 @@
-coffee='node_modules/.bin/coffee'
-forever='node_modules/.bin/forever'
-
-uptime_conf='--minUptime 5000 --spinSleepTime 5000'
-log_conf='-a -o log/std.log -e log/err.log'
-$forever start $uptime_conf $log_conf -c $coffee $app
